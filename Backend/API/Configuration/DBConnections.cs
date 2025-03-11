5cad795a35dcb20f28d5d4a08b1f23e70c65f788
SuazoXD <suazocarlos50@gmail.com>
2025-03-11 10:37:21 -0600

    Feature: agregando migraciones al proyecto

--- Backend/API/Configuration/DBConnections.cs
+++ Backend/API/Configuration/DBConnections.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Configuration
+﻿using Microsoft.Extensions.Configuration;
+
+namespace API.Configuration
 {
     public class DBConnections
     {
@@ -6,21 +8,18 @@
 
         public DBConnections()
         {
-            DotNetEnv.Env.Load();
-
             IConfiguration configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
 
             string dbServer = configuration["DB_HOST"] ?? "localhost";
-            string dbPort = configuration["DB_PORT"] ?? "1234";
-            string dbName = configuration["DB_NAME"] ?? "base";
-            string dbUser = configuration["DB_USER"] ?? "cons";
-            string dbPassword = configuration["DB_PASS"] ?? "sorto";
+            string dbPort = configuration["DB_PORT"] ?? "1433";
+            string dbName = configuration["DB_NAME"] ?? "MyDatabase";
+            string dbUser = configuration["DB_USER"] ?? "sa";
+            string dbPassword = configuration["DB_PASS"] ?? "YourStrong!Passw0rd";
 
             ConnectionString = $"Server={dbServer},{dbPort};Database={dbName};User Id={dbUser};Password={dbPassword};TrustServerCertificate=True;";
         }
     }
 }
-
