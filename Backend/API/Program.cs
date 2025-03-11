891677b93a39cebc236bfec14f5519a06ed5be21
YojanErazo <yojanerazo07@gmail.com>
2025-03-11 13:49:17 -0600

    Se ha agregado el controlador para product

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,8 +1,8 @@
-using API.Configuration;
-using Aplication.Interfaces;
-using Aplication.Services;
-using Infrastructure.Persistence;
+using Application.Interfaces;
+using Application.Services;
 using Infrastructure.Repositories;
+using Infrastructure.Persistence;
+using API.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,8 +12,9 @@ var dbConfig = new DBConnections();
 builder.Services.AddDbContext<ProjectDBContext>(options =>
     options.UseSqlServer(dbConfig.ConnectionString));
 
-// Registro de repositorios y otros servicios
+// Registro de repositorios y servicios
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -32,3 +33,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
