6137774327ea95df62266dbc2b3cde6c1b2ea9a7
SuazoXD <suazocarlos50@gmail.com>
2025-03-27 13:59:50 -0600

    Feature: Se agrego permiso

---
+++ Backend/Domain/Entities/Permiso.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Permiso : AggregateRoot
+    {
+        public int Id { get; private set; }
+        public string Descripcion { get; private set; }
+
+        protected Permiso() { }
+
+        public Permiso(string descripcion)
+        {
+            Descripcion = descripcion;
+        }
+        public void UpdateDescripcion(string descripcion)
+        {
+            Descripcion = descripcion;
+        }
+    }
+}
