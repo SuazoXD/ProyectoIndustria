3dd07be21903edf5338fc7353ed521a9dbfe6496
YojanErazo <yojanerazo07@gmail.com>
2025-03-27 19:27:50 -0600

    feature: se han agregado las clases Archivo y ArchivoLR

---
+++ Backend/Domain/Entities/ArchivoListaReproduccion.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+   public class ArchivoListaReproduccion : AggregateRoot
+    {
+        public int IdLista { get; private set; }
+        public int IdArchivo { get; private set; }
+
+        public ListaDeReproduccion ListaDeReproduccion { get; private set; }
+        public Archivo Archivo { get; set; }
+        protected ArchivoListaReproduccion() { }
+
+        public ArchivoListaReproduccion(int idLista, int idArchivo)           
+        {
+            IdLista = idLista;
+            IdArchivo = idArchivo;
+        }
+    }
+
+    
+
+}
