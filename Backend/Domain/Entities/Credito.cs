a09fd071f88c6902a44dd6d1e814c6744834158c
YojanErazo <yojanerazo07@gmail.com>
2025-03-27 23:48:58 -0600

    feature: agregando las entidades Credito y ContenidoPremium

---
+++ Backend/Domain/Entities/Credito.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Credito : AggregateRoot
+    {
+        public int IdUsuario { get; private set; }
+        public int Cantidad { get; private set; }
+        public DateTime FechaAdquisicion { get; private set; }
+
+        public Usuario Usuario { get; set; }
+
+        protected Credito() { }
+
+        public Credito (int idUsuario, int cantidad)
+        {
+            IdUsuario = idUsuario;
+            Cantidad = cantidad;
+            FechaAdquisicion = DateTime.UtcNow;
+        }
+    }
+}
