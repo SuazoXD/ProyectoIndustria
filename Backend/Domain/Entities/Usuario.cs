14188340a6e2f0dbe9ef1a510d961ae92c45d324
SuazoXD <suazocarlos50@gmail.com>
2025-03-27 23:56:49 -0600

    Feature: Se agrego entity Usuario

---
+++ Backend/Domain/Entities/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Common;
+using System;
+
+namespace Domain.Entities
+{
+    public class Usuario : AggregateRoot
+    {
+        public int Id { get; private set; }
+        public string NombreUsuario { get; private set; }
+        public string Contrasenia { get; private set; }
+        public string Correo { get; private set; }
+        public int IdRol { get; private set; }
+        public DateTime FechaRegistro { get; private set; }
+
+        public Rol Rol { get; set; }
+
+        public ICollection<Archivo> Archivos { get; set; }
+        public ICollection<Pago> Pagos { get; set; }
+        public ICollection<Credito> Creditos { get; set; }
+        public ICollection<ContenidoPremium> ContenidosPremium { get; set; }
+        public ICollection<Privacidad> Privacidades { get; set; }
+        public ICollection<ListaDeReproduccion> ListasDeReproduccion { get; set; }
+        public ICollection<Favorito> Favoritos { get; set; }
+
+        protected Usuario() { }
+
+        public Usuario(string nombreUsuario, string contrasenia, string correo, int idRol)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasenia = contrasenia;
+            Correo = correo;
+            IdRol = idRol;
+            FechaRegistro = DateTime.UtcNow;
+        }
+    }
+}
