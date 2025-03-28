a669c113a5a015dab4761ae67e13a6e75aac096f
SuazoXD <suazocarlos50@gmail.com>
2025-03-28 00:04:01 -0600

    feature: se ha agregado la entidad

---
+++ Backend/Domain/Entities/Rol.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class Rol : AggregateRoot
+    {
+        public int Id { get; private set; }
+        public string NombreRol { get; private set; }
+        public string Descripcion { get; private set; }
+
+        public ICollection<Usuario> Usuarios { get; set; }
+
+        protected Rol()
+        {
+            Usuarios = new List<Usuario>();
+        }
+
+        public Rol(string nombreRol, string descripcion)
+        {
+            NombreRol = nombreRol;
+            Descripcion = descripcion;
+            Usuarios = new List<Usuario>();
+        }
+    }
+}
