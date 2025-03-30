d3cbdaf50b09bc9c0d3185a5aeef4820321749a4
Stefany Cruz <stefanycruz1726@gmail.com>
2025-03-30 11:52:18 -0600

    feature: se creo el dto de factura

---
+++ Backend/Aplication/DTOs/Factura/FacturaRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.Factura
+{
+    public class FacturaRequestDTO
+    {
+        public int IdPago { get; set; }
+        public int NumeroFactura { get; set; }
+        public decimal TotalPagar { get; set; }
+        public string EstadoFactura { get; set; }  // Ej. "pendiente"
+    }
+}
