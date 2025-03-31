485308312b144c5f3c091808b6f8f528b10ea788
Stefany Cruz <stefanycruz1726@gmail.com>
2025-03-31 15:45:17 -0600

    feature se agrego request y response dto de pago

---
+++ Backend/Aplication/DTOs/Pago/PagoRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs.Pago
+{
+    public class PagoRequestDTO
+    {
+        public decimal Monto { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaPago { get; set; }
+        public int IdUsuario { get; set; }
+        public int MetodoPago { get; set; }
+    }
+}
---
+++ Backend/Aplication/DTOs/Pago/PagoResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Application.DTOs.Pago
+{
+    public class PagoResponseDTO
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int MetodoPago { get; set; }
+        public decimal Monto { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaPago { get; set; }
+    }
+}
