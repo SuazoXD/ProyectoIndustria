f3bead7576b25fc55c033b75f3382cf0cbabde09
Stefany Cruz <stefanycruz1726@gmail.com>
2025-03-31 15:16:51 -0600

    feature: se agrego listadereprosuccion response y request

---
+++ Backend/Aplication/DTOs/ListaDeReproduccion/ListaDeReproduccionRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.ListaDeReproduccion
+{
+    public class ListaDeReproduccionRequestDTO
+    {
+        public int IdUsuario { get; set; }
+        public string NombreLista { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
