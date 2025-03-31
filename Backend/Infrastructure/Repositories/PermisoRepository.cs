99bec25feaf03480b7bf1e836e03af4c993198ce
SuazoXD <suazocarlos50@gmail.com>
2025-03-30 21:40:39 -0600

    Feature: Se agrego el PermisoRepository

---
+++ Backend/Infrastructure/Repositories/PermisoRepository.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class PermisoRepository : IPermisoRepository
+    {
+        private readonly ProjectDBContext _context;
+
+        public PermisoRepository(ProjectDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Permiso>> GetAllAsync() =>
+            await _context.Permisos.ToListAsync();
+
+        public async Task<Permiso> GetByIdAsync(int id) =>
+            await _context.Permisos.FindAsync(id);
+
+        public async Task<Permiso> CreateAsync(Permiso permiso)
+        {
+            _context.Permisos.Add(permiso);
+            await _context.SaveChangesAsync();
+            return permiso;
+        }
+
+        public async Task<bool> UpdateAsync(Permiso permiso)
+        {
+            _context.Permisos.Update(permiso);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var permiso = await _context.Permisos.FindAsync(id);
+            if (permiso == null)
+                return false;
+            _context.Permisos.Remove(permiso);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
