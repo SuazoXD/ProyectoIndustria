d9648b5bf365b1484209ac685de6f5704abba189
Stefany Cruz <stefanycruz1726@gmail.com>
2025-03-31 15:37:17 -0600

    feature:se creo el response y request del dto de metodopago

---
+++ Backend/Aplication/DTOs/MetodoPago/MetodoPagoRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.MetodoPago
+{
+    public class MetodoPagoRequestDTO
+    {
+        public string NombreMetodo { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ Backend/Aplication/DTOs/MetodoPago/MetodoPagoResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.MetodoPago
+{
+    public class MetodoPagoResponseDTO
+    {
+        public int Id { get; set; }
+        public string NombreMetodo { get; set; }
+        public bool Activo { get; set; }
+    }
+}
