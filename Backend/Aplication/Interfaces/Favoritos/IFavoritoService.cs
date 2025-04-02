e02ed3624c7128ac37b28af9ff3dd4bc9bf9ab8d
Stefany Cruz <stefanycruz1726@gmail.com>
2025-04-02 12:52:17 -0600

    ADD FEATURE: Interfaces Favoritos

---
+++ Backend/Aplication/Interfaces/Favoritos/IFavoritoService.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.Favorito;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Favoritos
+{
+    public interface IFavoritoService
+    {
+        Task<IEnumerable<FavoritoResponseDTO>> GetAllAsync();
+        Task<FavoritoResponseDTO> GetByIdAsync(int id);
+        Task<FavoritoResponseDTO> CreateAsync(FavoritoRequestDTO dto);
+        Task<bool> UpdateAsync(int id, FavoritoRequestDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
