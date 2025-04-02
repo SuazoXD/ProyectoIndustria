94ea4b45020685f4b9c06447dd1773938c1333ad
YojanErazo <yojanerazo07@gmail.com>
2025-04-02 14:16:01 -0600

    ADD FEATURE: Aplication Interfaces Archivo y ArchivoLR

---
+++ Backend/Aplication/Interfaces/Archivos/IArchivoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.AggregateRoots;
+
+namespace Aplication.Interfaces.Archivos
+{
+    public interface IArchivoRepository
+    {
+        Task<IEnumerable<Archivo>> GetAllAsync();
+        Task<Archivo> GetByIdAsync(int id);
+        Task<Archivo> CreateAsync(Archivo archivo);
+        Task<bool> UpdateAsync(Archivo archivo);
+        Task<bool> DeleteAsync(int id);
+    }
+}
+
---
+++ Backend/Aplication/Interfaces/ArchivosListaReproduccion/IArchivoListaReproduccionRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Domain.Entities;
+
+namespace Aplication.Interfaces.ArchivosListaReproduccion
+{
+    public interface IArchivoListaReproduccionRepository
+    {
+        Task<IEnumerable<ArchivoListaReproduccion>> GetAllAsync();
+        Task<ArchivoListaReproduccion> GetByIdAsync(int id);
+        Task<ArchivoListaReproduccion> CreateAsync(ArchivoListaReproduccion entity);
+        Task<bool> UpdateAsync(ArchivoListaReproduccion entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
