d295f25df391e36fbe7622a820206513c1a69563
SuazoXD <suazocarlos50@gmail.com>
2025-04-02 14:14:34 -0600

    ADD FEATURE: PERMISOS PRIVACIDAD ROLSERVICES AND USUARIOS

---
+++ Backend/Aplication/Services/Permisos/PermisoService.cs
@@ -0,0 +1,68 @@
+﻿using Application.DTOs.Permiso;
+using Application.Interfaces.Permisos;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Services.Permisos
+{
+    public class PermisoService : IPermisoService
+    {
+        private readonly IPermisoRepository _permisoRepository;
+
+        public PermisoService(IPermisoRepository permisoRepository)
+        {
+            _permisoRepository = permisoRepository;
+        }
+
+        public async Task<IEnumerable<PermisoResponseDTO>> GetAllAsync()
+        {
+            var permisos = await _permisoRepository.GetAllAsync();
+            return permisos.Select(p => new PermisoResponseDTO
+            {
+                Id = p.Id,
+                Descripcion = p.Descripcion
+            });
+        }
+
+        public async Task<PermisoResponseDTO> GetByIdAsync(int id)
+        {
+            var permiso = await _permisoRepository.GetByIdAsync(id);
+            if (permiso == null) return null;
+            return new PermisoResponseDTO
+            {
+                Id = permiso.Id,
+                Descripcion = permiso.Descripcion
+            };
+        }
+
+        public async Task<PermisoResponseDTO> CreateAsync(PermisoRequestDTO dto)
+        {
+            var permiso = new Permiso(dto.Descripcion);
+            var created = await _permisoRepository.CreateAsync(permiso);
+            return new PermisoResponseDTO
+            {
+                Id = created.Id,
+                Descripcion = created.Descripcion
+            };
+        }
+
+        public async Task<bool> UpdateAsync(int id, PermisoRequestDTO dto)
+        {
+            var permiso = await _permisoRepository.GetByIdAsync(id);
+            if (permiso == null)
+                return false;
+
+            // Usar el método UpdateDescripcion en lugar de asignar directamente la propiedad
+            permiso.UpdateDescripcion(dto.Descripcion);
+
+            return await _permisoRepository.UpdateAsync(permiso);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _permisoRepository.DeleteAsync(id);
+        }
+    }
+}
