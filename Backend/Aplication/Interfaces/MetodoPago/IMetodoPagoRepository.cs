bf171fe66a5a94b303248c61c3fe803496e25e6b
Stefany Cruz <stefanycruz1726@gmail.com>
2025-04-02 13:23:05 -0600

    ADD FEATURE: Interface MetodoPago

---
+++ Backend/Aplication/Interfaces/MetodoPago/IMetodoPagoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.MetodosPago
+{
+    public interface IMetodoPagoRepository
+    {
+        Task<IEnumerable<MetodoPago>> GetAllAsync();
+        Task<MetodoPago> GetByIdAsync(int id);
+        Task<MetodoPago> CreateAsync(MetodoPago metodoPago);
+        Task<bool> UpdateAsync(MetodoPago metodoPago);
+        Task<bool> DeleteAsync(int id);
+    }
+}
