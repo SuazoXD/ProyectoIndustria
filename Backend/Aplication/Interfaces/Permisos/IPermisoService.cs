3f480417c7f94932135cc23e8e5db83d483163d5
SuazoXD <suazocarlos50@gmail.com>
2025-04-02 12:34:51 -0600

    ADD FEATURE: Aplication Interfaces Permisos, Privacidades

---
+++ Backend/Aplication/Interfaces/Permisos/IPermisoService.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.Permisos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Permisos
+{
+    public interface IPermisoService
+    {
+        Task<IEnumerable<PermisoResponseDTO>> GetAllAsync();
+        Task<PermisoResponseDTO> GetByIdAsync(int id);
+        Task<PermisoResponseDTO> CreateAsync(PermisoRequestDTO dto);
+        Task<bool> UpdateAsync(int id, PermisoRequestDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
